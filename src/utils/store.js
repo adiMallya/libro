943d01d39934fb56a72298cb1de9710c6cd32053
adiMallya <adimallya99@gmail.com>
2023-06-17 20:30:30 +0530

    add : books data

---
+++ src/utils/store.js
@@ -0,0 +1,63 @@
+export const books = [
+    {
+        title: "To Kill a Mockingbird",
+        author: "Harper Lee",
+        imgURL: "",
+        category: "Read"
+    },
+    {
+        title: "1984",
+        author: "George Orwell",
+        imgURL: "",
+        category: "Read"
+    },
+    {
+        title: "Pride and Prejudice",
+        author: "Jane Austen",
+        imgURL: "",
+        category: "Read"
+    },
+    {
+        title: "The Great Gatsby",
+        author: "F. Scott Fitzgerald",
+        imgURL: "",
+        category: "Want to Read"
+    },
+    {
+        title: "The Catcher in the Rye",
+        author: "J.D. Salinger",
+        imgURL: "",
+        category: "Currently Reading"
+    },
+    {
+        title: "Harry Potter and the Sorcerer's Stone",
+        author: "J.K. Rowling",
+        imgURL: "",
+        category: "Want to Read"
+    },
+    {
+        title: "The Hobbit",
+        author: "J.R.R. Tolkien",
+        imgURL: "",
+        category: "Want to Read"
+    },
+    {
+        title: "The Alchemist",
+        author: "Paulo Coelho",
+        imgURL: "",
+        category: "Currently Reading"
+    },
+    {
+        title: "To Kill a Kingdom",
+        author: "Alexandra Christo",
+        imgURL: "",
+        category: "Currently Reading"
+    },
+    {
+        title: "The Book Thief",
+        author: "Markus Zusak",
+        imgURL: "",
+        category: "Read"
+    }
+];
+
