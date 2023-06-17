e1d74a1e50d51a64d98a0d1d51fa36fb513390f6
adiMallya <adimallya99@gmail.com>
2023-06-17 20:31:13 +0530

    add : state mgmnt

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import App from "./App.jsx";
+import { DataProvider } from "src/contexts";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <DataProvider>
+        <App />
+      </DataProvider>
     </Router>
   </React.StrictMode>
 );
