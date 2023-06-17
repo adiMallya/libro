e3557b8262436697944d5fe22cf716162c3fae25
adiMallya <adimallya99@gmail.com>
2023-06-18 00:23:58 +0530

    refact : initialize data

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home, Search } from "src/pages";
 import "./App.css";
+import { useData } from "src/contexts";
+import { books, categories } from "src/utils/store";
 
 function App() {
+  const { dispatch } = useData();
+
+  useEffect(() => {
+    dispatch({ type: "INIT_DATA", payload: books });
+    dispatch({ type: "INIT_CATEGORIES", payload: categories });
+  }, []);
+
   return (
     <>
       <Routes>
